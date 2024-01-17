1361cf7e0663697eb40020e0406efefda3ca73cf
SD-Keish <155769912+SD-Keish@users.noreply.github.com>
2024-01-17 07:40:52 -0600

    Add files via upload
    Autopilot upload Hardware Hash script.

---
+++ Autopilot.ps1
@@ -0,0 +1,5 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+PowerShell.exe -ExecutionPolicy Bypass
+Install-Script -name Get-WindowsAutopilotInfo -Force
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Get-WindowsAutopilotInfo -Online
